4ac2f54cf14da9508196b65da6ab1197b216c803
FatherOfJumong <zedgozedgo15@gmail.com>
2024-07-31 23:05:22 +0400

    fix file location

--- python_assignment/main.py
+++ python_assignment/main.py
@@ -4,21 +4,23 @@ import os
 from utils import load_and_filter_data, process_contracts
 from src.feature_generation import generate_features
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-io_path = os.path.join(script_dir, "data")
-
 def main():
     """Main function to run the project."""
+    # Define paths
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(script_dir, "data")
+    input_file = os.path.join(data_dir, "data.csv")
+    output_file = os.path.join(data_dir, "contract_features.csv")
+
     # loading and filtering data
-    df = load_and_filter_data(os.path.join(io_path, "data.csv"))
+    df = load_and_filter_data(input_file)
     # processing contracts
     df_processed = process_contracts(df)
     # generating features
     df_features = generate_features(df_processed)
     # saving features
-    output_path = os.path.join(io_path, "contract_features.csv")
-    df_features.to_csv(output_path, index=False)
-    print(f"Features generated and saved to '{output_path}'")
+    df_features.to_csv(output_file, index=False)
+    print(f"Features generated and saved to '{output_file}'")
     
 if __name__ == "__main__":
     main()
