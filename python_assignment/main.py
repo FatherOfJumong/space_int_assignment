b05fa77dc78e5d2fd9c1311c2e9f42e9b1d2396b
FatherOfJumong <zedgozedgo15@gmail.com>
2024-07-31 22:58:43 +0400

    refactor file path

--- python_assignment/main.py
+++ python_assignment/main.py
@@ -1,22 +1,24 @@
 """ Main script to run the project. """
 
-
+import os
 from utils import load_and_filter_data, process_contracts
 from src.feature_generation import generate_features
 
-io_path = "./python_assignment/data"
+script_dir = os.path.dirname(os.path.abspath(__file__))
+io_path = os.path.join(script_dir, "data")
 
 def main():
     """Main function to run the project."""
-    # loding and filtering data
-    df = load_and_filter_data(f"{io_path}/data.csv")
+    # loading and filtering data
+    df = load_and_filter_data(os.path.join(io_path, "data.csv"))
     # processing contracts
     df_processed = process_contracts(df)
     # generating features
     df_features = generate_features(df_processed)
     # saving features
-    df_features.to_csv(f"{io_path}/contract_features.csv", index=False)
-    print(f"Features generated and saved to '{io_path}/contract_features.csv'")
+    output_path = os.path.join(io_path, "contract_features.csv")
+    df_features.to_csv(output_path, index=False)
+    print(f"Features generated and saved to '{output_path}'")
     
 if __name__ == "__main__":
     main()
